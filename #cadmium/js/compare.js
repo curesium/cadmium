893c5133200785fda48316dbca9b5af3a689624d
Thijs Boom <twboom@hotmail.nl>
2021-01-11 15:05:33 +0100

    Added submit button

---
+++ #cadmium/js/compare.js
@@ -0,0 +1,5 @@
+let input1 = document.quarySelector('#input1');
+let input2 = document.quarySelector('#input2');
+
+let submit = document.quarySelector('#submitButton');
+submit.addEventListener('click', compare);
